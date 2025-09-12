5abfd19f1b1b44788b7239aaa758cfb86822482c
Starway <148691101+CodeLikeBA56@users.noreply.github.com>
2025-09-12 05:59:44 +0500

    Contact.js
    Created a separate script for Contact us form.

--- js/script.js
+++ js/script.js
@@ -66,59 +66,6 @@ main.onscroll = () => {
   });
 };
 
-/*========== Contact Form Validation Check ==========*/
-
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-formBtn.onclick = function (event) {
-  event.preventDefault();
-  sendEmail();
-};
-
-for (let i = 0; i < formInputs.length; i++) { // add event to all form input field
-  formInputs[i].addEventListener("input", function () {
-    if (form.checkValidity()) // check form validation
-      formBtn.removeAttribute("disabled");
-    else
-      formBtn.setAttribute("disabled", "");
-  });
-}
-
-/*========== Send email through EmailJs ==========*/
-
-const senderName = document.getElementById("name");
-const senderEmail = document.getElementById("email");
-const senderMessage = document.getElementById("message");
-
-function sendEmail() {
-  const name = senderName.value.trim();
-  const email = senderEmail.value.trim();
-  const message = senderMessage.value.trim();
-  
-  if (!name || !email || !message) {
-    alert("Please fill in all fields.");
-    return;
-  }
-  
-  const serviceId = "service_gasm89q";
-  const templateId = "template_ztctj1z";
-  const params = { name, email, message };
-  
-  emailjs.send(serviceId, templateId, params, "FZLmraz64Z3C4k5Ko")
-  .then(res => {
-    alert("Message sent successfully!");
-    senderName.value = "";
-    senderEmail.value = "";
-    senderMessage.value = "";
-  })
-  .catch(error => {
-    console.error("EmailJS error:", error);
-    alert("Oops! Something went wrong. Try again later.");
-  });
-}
-
 /*========== Display Projects ==========*/
 import projects from './projects.js';
 
